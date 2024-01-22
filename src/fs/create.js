b250bba5e4848055f31f27595685f9cecd37aee7
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-22 11:38:35 +0700

    feat: add solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import {writeFile} from 'fs/promises';
+import {resolve, dirname} from 'path';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const filePath = resolve(dirname(''), 'fs', 'files', 'fresh.txt');
+        await writeFile(filePath, 'I am fresh and young', {
+            flag: 'wx',
+        });
+    } catch {
+        throw Error('I am fresh and young');
+    }
 };
 
 await create();
