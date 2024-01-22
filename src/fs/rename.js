60200af24090ed1a645e7a41cecafb28685d781e
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-22 12:10:57 +0700

    feat: add solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import {rename as fsRename, access, constants} from 'fs/promises';
+import {resolve, dirname} from 'path';
+
+const checkFileNotExists = async (path) => {
+    try {
+        await access(path, constants.R_OK);
+    } catch {
+        return;
+    }
+    throw Error('File exists');
+}
+
 const rename = async () => {
-    // Write your code here 
+    const dir = resolve(dirname(''), 'fs', 'files');
+    const oldPath = resolve(dir, 'wrongFilename.txt');
+    const newPath = resolve(dir, 'properFilename.md');
+    try {
+        await checkFileNotExists(newPath);
+        await fsRename(oldPath, newPath);
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
 await rename();
