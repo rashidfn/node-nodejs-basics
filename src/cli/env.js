caf931742d304cefdfbff51cb83f3a077f63f232
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-22 14:46:25 +0700

    feat: add solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+
+process.env.RSS_name1 = 'value1';
+process.env.RSS_name2 = 'value2';
+
 const parseEnv = () => {
-    // Write your code here 
+    const entries = Object.entries(process.env).filter(([key]) => key.startsWith('RSS_'));
+    entries.forEach(([key, value], index) => {
+        const end = index === entries.length - 1 ? '' : '; ';
+        process.stdout.write(`${key}=${value}${end}`);
+    })
 };
 
 parseEnv();
