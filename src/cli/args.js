5a306bc90732ad48b053b1fa06592f9572a66237
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-22 15:00:47 +0700

    feat: add solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((arg, index) => {
+        const prefix = index > 0 ? ', ' : '';
+        if (arg.startsWith('--')) {
+            process.stdout.write(`${prefix}${arg.slice(2)} is ${args[index + 1]}`);
+        }
+    });
 };
 
 parseArgs();
