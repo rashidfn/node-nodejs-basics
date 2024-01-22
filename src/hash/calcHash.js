0f9c7eea49b6be86c9eab74975a6d77aa2ad829a
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-22 17:29:09 +0700

    feat: add solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import {createReadStream} from 'fs';
+import {dirname, resolve} from 'path';
+import {createHash} from 'crypto';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const dir = resolve(dirname(''), 'hash', 'files', 'fileToCalculateHashFor.txt');
+    const stream = createReadStream(dir);
+    stream.on('readable', () => {
+        const data = stream.read();
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    })
 };
 
 await calculateHash();
