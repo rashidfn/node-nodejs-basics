164b3568af873800f0362feea0b48e0a65624f53
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-23 15:30:31 +0700

    feat: add solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import {createReadStream} from "fs";
+import {dirname, resolve} from "path";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = resolve(dirname(''), 'streams', 'files', 'fileToRead.txt');
+    const stream = createReadStream(filePath);
+    stream.on('readable', () => {
+        const data = stream.read();
+        if (data) {
+            process.stdout.write(data);
+        }
+    })
 };
 
 await read();
