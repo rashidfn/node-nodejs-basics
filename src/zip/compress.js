f9a0470248e184ed42ab9a532f6af68f54802d5b
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-23 21:24:53 +0700

    feat: add solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import {dirname, resolve} from "path";
+import {createGzip} from 'zlib';
+import {createReadStream, createWriteStream} from "fs";
+import {pipeline} from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const input = resolve(dirname(''), 'zip', 'files', 'fileToCompress.txt');
+    const output = resolve(dirname(''), 'zip', 'files', 'archive.gz');
+
+    const zip = createGzip();
+    const readStream = createReadStream(input);
+    const writeStream = createWriteStream(output);
+
+    return new Promise((resolve, reject) => {
+        pipeline(readStream, zip, writeStream, (err) => {
+           if (!err) {
+               resolve();
+           }
+           reject();
+        })
+    })
 };
 
 await compress();
