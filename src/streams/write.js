89fbd906ef622eb01635957f9ea38bc2a8afc01a
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-23 21:12:44 +0700

    feat: add solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import {dirname, resolve} from "path";
+import {createWriteStream} from "fs";
+import {Readable} from 'stream';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = resolve(dirname(''), 'streams', 'files', 'fileToWrite.txt');
+    const stream = createWriteStream(filePath);
+    process.stdin.on('data', (data) => {
+        if (data) {
+            stream.write(data);
+        }
+    })
+
 };
 
 await write();
