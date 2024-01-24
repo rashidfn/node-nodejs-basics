497b466da35e5f42bfeac10c5fdd4e651204e99c
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-24 23:31:25 +0700

    feat: add solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import {Worker} from 'worker_threads';
+import {cpus} from 'os';
+import {dirname, resolve} from "path";
+
+const getPromises = () => {
+    const cpuCount = cpus().length;
+    let start = 10;
+    const workerPath = resolve(dirname(''), 'wt', 'worker.js');
+
+    return Array.from({length: cpuCount}, () => (
+        new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: start,
+            });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error(code));
+            });
+            start++;
+        })
+    ));
+}
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const promiseResults = await Promise.allSettled(getPromises());
+    const result = promiseResults.map(({status, value}) => {
+        return {
+            status: status === 'fulfilled' ? 'resolved' : 'error',
+            data: value,
+        }
+    });
+    console.log(result);
 };
 
 await performCalculations();
