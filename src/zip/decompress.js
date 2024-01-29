9ab4444c6d255b191d2c984f77ecc24ca0c99a2b
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-29 22:13:29 +0700

    feat: add solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import {createGunzip, deflate, unzip, gzip} from 'zlib';
+import {dirname, resolve} from "path";
+import {createReadStream, createWriteStream} from "fs";
+import {pipeline} from "stream";
+
 const decompress = async () => {
-    // Write your code here 
+    const input = resolve(dirname(''), 'zip', 'files', 'archive.gz');
+    const output = resolve(dirname(''), 'zip', 'files', 'fileToCompress.txt');
+
+    const zip = createGunzip();
+    const readStream = createReadStream(input);
+    const writeStream = createWriteStream(output);
+
+    return new Promise((resolve, reject) => {
+        pipeline(readStream, zip, writeStream, (err) => {
+           if (!err) {
+               resolve();
+           }
+           reject();
+        });
+    })
 };
 
 await decompress();
