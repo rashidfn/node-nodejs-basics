cd20e5f5d66d3ef3f2188b485504e2472bc435e5
rashidfn <zhumadilov.rashid@gmail.com>
2024-01-29 22:18:45 +0700

    feat: add solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform, pipeline } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    const stream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('');
+            this.push(reversed);
+            callback();
+        }
+    });
+    return new Promise((resolve, reject) => {
+        pipeline(process.stdin, stream, process.stdout, (err) => {
+           if (!err) {
+               resolve();
+           }
+           reject();
+        });
+    })
 };
 
 await transform();
